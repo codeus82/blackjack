c4ef8e9dfec8a525e02931fc18f7a249787ec8aa
unknown <email@example.com>
2020-03-23 10:43:18 -0700

    added files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log("Working");
+
+let dealer = {
+    cards = [],
+    points = 0,
+}
+let player = {
+    cards = [],
+    points = 0,
+}
+
+let pushedCard = document.getElementById(".dealer-card-1");
+console.log(pushedCard);
