693a17ba040096a9bc35cc2144297ba0e0769c05
unknown <email@example.com>
2020-03-23 15:35:12 -0700

    added fisher's algorithm and deck.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,44 @@
 console.log("Working");
 
-let dealer = {
-    cards = [],
-    points = 0,
-}
-let player = {
-    cards = [],
-    points = 0,
+console.log(deck);
+
+let playerHand = [];
+let dealerHand = [];
+
+//Fisher-Yate's algorithm
+function shuffle(deck) {
+  let i = 0,
+    j = 0,
+    temp = null;
+  for (i = deck.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
 }
 
-let pushedCard = document.getElementById(".dealer-card-1");
-console.log(pushedCard);
+function initialHand() {
+  shuffle(deck);
+  for (let i = 0; i < 2; i++) {
+    playerHand.push(deck.shift());
+    dealerHand.push(deck.shift());
+  }
+  return playerHand;
+}
+
+console.log(playerHand);
+
+let firstCard = deck.pop();
+
+let addCard = document.getElementById("dealer-hand");
+
+let hitTest = document.getElementById("hit");
+hitTest.addEventListener("click", function() {
+  console.log("click");
+});
+
+let stayTest = document.getElementById("stay");
+stayTest.addEventListener("click", function(e) {
+  console.log("clicked");
+});
